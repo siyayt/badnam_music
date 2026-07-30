8b826e65a83e957f604d4caccfee1e9a17128541
siyayt <rajayt0825@gmail.com>
2026-07-30 11:50:32 +0530

    Update play.py

--- ShiviMusic/utils/inline/play.py
+++ ShiviMusic/utils/inline/play.py
@@ -82,7 +82,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
          [
              InlineKeyboardButton(text="- 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.PRIMARY),
-             InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/Only_badnam?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙", style=ButtonStyle.SUCCESS),
+             InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/BADMASHXD?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙", style=ButtonStyle.SUCCESS),
              InlineKeyboardButton(text="𝟤𝟢 s + ", callback_data="seek_forward_20", style=ButtonStyle.PRIMARY)
          ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER),
@@ -103,7 +103,7 @@ def stream_markup(_, chat_id):
          ],
         [
              InlineKeyboardButton(text="- 𝟤𝟢 s", callback_data="seek_backward_20", style=ButtonStyle.PRIMARY),
-             InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/Only_badnam?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙", style=ButtonStyle.SUCCESS),
+             InlineKeyboardButton(text="ᴘʀᴏᴍᴏ", url=f"https://t.me/BADMASHXD?text=𝖧ᴇʏ%20ʙᴀʙʏ%20%20😄%20ɪ%20ᴡᴀɴᴛ%20ᴘᴀɪᴅ%20ᴘʀᴏᴍᴏᴛɪᴏɴ,%20ɢɪᴠᴇ%20ᴍᴇ%20ᴘʀɪᴄᴇ%20ʟɪsᴛ%20😙", style=ButtonStyle.SUCCESS),
              InlineKeyboardButton(text="𝟤𝟢 s+ ", callback_data="seek_forward_20", style=ButtonStyle.PRIMARY)
          ],
             [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER),
